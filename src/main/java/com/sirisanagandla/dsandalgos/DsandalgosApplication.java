7d8944c9fa6a70b350fab00cfdf6ec7f698a0d6b
Ramu Sirisanagandla <ramu.sirisanagandla@ASIDECHASEDIE.workdayinternal.com>
2019-05-02 22:56:00 -0700

    Initial Commit

---
+++ src/main/java/com/sirisanagandla/dsandalgos/DsandalgosApplication.java
@@ -0,0 +1,13 @@
+package com.sirisanagandla.dsandalgos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsandalgosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsandalgosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sirisanagandla/dsandalgos/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.sirisanagandla.dsandalgos;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Mr. Ramu");
+	}
+
+}
